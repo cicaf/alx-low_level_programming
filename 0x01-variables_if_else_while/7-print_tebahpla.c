ec7133dc4721ef65a5aeac80c763920237d57b35
cicaf <womarow@gmail.com>
2022-04-08 17:24:26 -0700

    Smile in the mirror ,Reversing the alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -prints the lowercase alphabet in reverse,
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char car;
+
+	for (car = 'z'; car >= 'a'; ch--)
+	{
+		putchar(car);
+	}
+	putchar('\n');
+	return (0);
+}
