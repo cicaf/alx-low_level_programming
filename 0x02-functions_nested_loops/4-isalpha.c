7dde0b6259e23c08130413be2c9e638045fe3271
o_w <womarow@gmail.com>
2022-04-13 01:44:58 +0300

    add these and see

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ *
+ * _isalpha - is the new code entrance
+ * @c: is the parameter.
+ * Return: either 1 or 0 also binary.
+ */
 int _isalpha(int c)
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
