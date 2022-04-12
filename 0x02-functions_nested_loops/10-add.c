516b0bdd73f194e8345183009fae82adcc47ee36
o_w <womarow@gmail.com>
2022-04-13 02:09:53 +0300

    an adding function for 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ * add - an addition function.
+ * @x: int to be added first.
+ * @y: int to be added second.
+ * Return: gives you back the sum.
+ */
 int add(int x, int y)
 {
 	return (x + y);
