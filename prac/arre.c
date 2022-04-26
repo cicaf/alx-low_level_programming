fb9f0d39f00b686197a3616125244ef95544d26d
o_w <womarow@gmail.com>
2022-04-27 01:46:26 +0300

    a good commit but i need to read

---
+++ prac/arre.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	int a[98];
+	printf("a: %p \n", a);
+	printf("&a : %p",&a[0]);
+		return 0;
+
+
+
+}
---
+++ prac/parth.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int a[2];
+	int *p;
+	int n;
+
+	p = &n;
+
+	printf("p = &n;\n p: %p\n ",p);
+
+
+}
