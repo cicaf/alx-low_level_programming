09a241b5b1284d3a7d8090f5031d0ac5d70e98c7
o_w <womarow@gmail.com>
2022-04-29 04:43:04 +0300

    Dreams feel real while we're in them. It's only when we wake up that we realize something was actually strange

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,48 +1,18 @@
-int recursive_check(char *s1, char *s2, int wildC);
+#include "main.h"
+
 /**
- * wildcmp - uses  wildcard '*' and able to search
- *
- * @s1: the string 1 
- * @s2: the string 2
- *
- * Return: 1 if identical, 0 otherwise, -1 for error
- */
-int wildcmp(char *s1, char *s2)
+  * _strlen_recursion - Calculate the length of my string
+  * @s: the string
+  *
+  * Return: should give us an int
+  */
+int _strlen_recursion(char *s)
 {
-	return (recursive_check(s1, s2, 0));
-}
-/**
- * recursive_check - checks for wild cards recursively
- *
- * @s1: string 1 to check (no wild cards)
- * @s2: string to to check (can contain wildcards)
- * @wildC: counter to see if wildcard is present
- *
- * Return: 1 if identical, 0 for not, -1 for error
- */
-int recursive_check(char *s1, char *s2, int wildC)
-{
-	if (*s1 == '\0' && *s2 == '\0')
+	if (*s == '\0')
 	{
-		return (1);
-	}
-	if (*s2 == '*')
-		return (recursive_check(s1, s2 + 1, ++wildC));
-	else if (*s1 != *s2 && wildC > 0) /* skip to next rel char */
-	{
-		if (!*s1)
-			return (0);
-		return (recursive_check(s1 + 1, s2, wildC));
-	}
-	else if (*s1 == *s2 && wildC == 0) /* char match, next char for each str */
-		return (recursive_check(s1 + 1, s2 + 1, 0));
-	else if (*s1 == *s2 && wildC > 0)
-	{
-		if (!recursive_check(s1 + 1, s2 + 1, 0))
-			return (recursive_check(s1 + 1, s2, wildC));
-		return (recursive_check(s1 + 1, s2 + 1, wildC));
-	}
-	else if (*s1 != *s2 && wildC == 0) /* non matching and no wildC */
 		return (0);
-	return (-1); /* something unexpected occured, return -1 for error */
+	}
+
+	s++;
+	return (_strlen_recursion(s) + 1);
 }
