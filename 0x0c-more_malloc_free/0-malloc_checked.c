081a8a5ae2020fe52b923dd1ab18dc7188700170
o_w <womarow@gmail.com>
2022-05-11 05:07:41 +0300

    malloc again

--- 0x0c-more_malloc_free/0-malloc_checked.c
+++ 0x0c-more_malloc_free/0-malloc_checked.c
@@ -1,15 +1,17 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
-  * malloc_checked - Allocation with malloc.
-  * @b: Mem size.
-  *
-  * Return: Pointer to  memory.
-  */
+ * malloc_checked - Creates an array based on passed in value
+ * @b: Value passed in for array creation
+ * Return: nothing
+ */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr = malloc(b);
+	void *ptr = NULL;
+
+	ptr = malloc(b);
 
 	if (ptr == NULL)
 		exit(98);
