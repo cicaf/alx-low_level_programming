61de8600321a2a68f8a4822beeb8886299f6c34f
o_w <womarow@gmail.com>
2022-05-15 13:08:04 +0300

    POPPY

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+A non empty readme for the 0X0E-structurees-code
---
+++ 0x0E-structures_typedef/rae.c
@@ -0,0 +1,10 @@
+creeping with the girl next door...unadoo??? me nadoo??????
+sawa vim
+ni ile vim ya chooo?
+eeehhhh 
+mbona inakaa ivi
+sawa fast vim
+sawa vitu less
+sawa no bulk
+kama nini
+
