b4ef4716d931fe8119021f159dc691ddc78f6164
o_w <womarow@gmail.com>
2022-05-30 16:44:41 +0300

    blank

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,13 +1,10 @@
 #include <stdlib.h>
 #include "lists.h"
-
 /**
   * sum_listint - sums the data in the lists
   * @head: the kichwa of the list,genesis
-  * 
   * Return: sum of the list
   */
-
 int sum_listint(listint_t *head)
 {
 	int amount = 0;
