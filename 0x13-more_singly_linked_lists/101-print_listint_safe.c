4d8cb5adaf81da7d71e242a0b34283b6d53da7f3
o_w <womarow@gmail.com>
2022-05-30 17:59:10 +0300

    now

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,28 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "lists.h"
-
-/**
- * free_aux - list
- * @p: head.
- * Return: null.
- */
-void free_aux(aux_list *p)
-{
-	aux_list *aux;
-
-	if (p == NULL)
-	{
-		return;
-	}
-	while (p != NULL)
-	{
-		aux = p;
-		p = p->next;
-		free(aux);
-	}
-}
-
 /**
  * print_listint_safe - linked list.
  * @head: chwakis.
@@ -31,30 +8,7 @@ void free_aux(aux_list *p)
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t c = 0;
-	aux_list *a, *b, *t;
 
-	a = NULL;
-	for (; head != NULL; c++, head = head->next)
-	{
-		t = malloc(sizeof(aux_list));
-		if (!t)
-			exit(98);
-		t->p = (void *)head;
-		t->next = a;
-		a = t;
-		b = a->next;
-		while (b != NULL)
-		{
-			if (head == b->p)
-			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
-				free_aux(a);
-				return (c);
-			}
-			b = b->next;
-		}
-		printf("[%p] %d\n", (void *)head, head->n);
 	}
 	free_aux(a);
 	return (c);
--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
 /**
  * free_listint_safe - list safe.
