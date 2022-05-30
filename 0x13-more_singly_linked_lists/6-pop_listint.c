3397399634d28552d0d4560843d32380411ded27
o_w <womarow@gmail.com>
2022-05-30 16:33:51 +0300

    another one of the pop list int projects

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  * pop_listint - list the ints
+  * @head: penye chwakis iko hehe
+  *
+  *
+  *
+  * Return: Nothing,give us nothing...thats all we ask
+  */
+int pop_listint(listint_t **head)
+{
+	listint_t *new_head;
+	int n = 0;
+
+	if (*head != NULL)
+	{
+		new_head = (*head)->next;
+		n = (*head)->n;
+		free(*head);
+		*head = new_head;
+	}
+
+	return (n);
+}
