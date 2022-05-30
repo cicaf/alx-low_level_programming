080d3e815231e7b32823ca7fbbdde2140386c937
o_w <womarow@gmail.com>
2022-05-30 17:50:21 +0300

    i did something with  these last two damn...

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  * reverse_listint - list link reversal
+  * @head: pointer to first node
+  *
+  * Return: head of reversed node
+  */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL, *next = NULL;
+
+	if (head)
+	{
+		while (*head)
+		{
+			next = *head;
+			*head = (*head)->next;
+			next->next = prev;
+			prev = next;
+		}
+
+		*head = prev;
+		return (*head);
+	}
+
+	return (NULL);
+}
+
+
---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,59 @@
+#include "lists.h"
+
+/**
+ * free_aux - list
+ * @p: head.
+ *
+ * Return: null.
+ */
+void free_aux(aux_list *p)
+{
+	aux_list *aux;
+
+	if (p == NULL)
+	{
+		return;
+	}
+	while (p != NULL)
+	{
+		aux = p;
+		p = p->next;
+		free(aux);
+	}
+}
+
+/**
+ * print_listint_safe - linked list.
+ * @head: chwakis.
+ *
+ * Return: total nodes
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t c = 0;
+	aux_list *a, *b, *t;
+
+	a = NULL;
+	for (; head != NULL; c++, head = head->next)
+	{
+		t = malloc(sizeof(aux_list));
+		if (!t)
+			exit(98);
+		t->p = (void *)head;
+		t->next = a;
+		a = t;
+		b = a->next;
+		while (b != NULL)
+		{
+			if (head == b->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_aux(a);
+				return (c);
+			}
+			b = b->next;
+		}
+		printf("[%p] %d\n", (void *)head, head->n);
+	}
+	free_aux(a);
+	return (c);
